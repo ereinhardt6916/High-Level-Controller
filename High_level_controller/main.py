f820d9c6146a9c4a7b47e80e9d4d2a9ed1ff7195
Weipeng Tang <samwisetang@hotmail.com>
2021-06-20 11:29:42 -0400

    add class for xy system

--- High_level_controller/main.py
+++ High_level_controller/main.py
@@ -2,7 +2,9 @@ import I2C_LCD_driver
 import RPi.GPIO as GPIO
 from encoder import Encoder
 from piece_locator import Piece_locator
+from xy_selector import XY_selector
 import time
+import logging
 
 # variables for LCD
 mylcd = I2C_LCD_driver.lcd()
@@ -14,29 +16,38 @@ GPIO.setmode(GPIO.BCM)
 port_PL = "/dev/ttyACM0"
 pl = Piece_locator(port_PL)
 
+# variables for xy selector
+port_XY = "/dev/ttyACM1"
+xy = XY_selector(port_XY)
+
 def valueChanged(value):
-    print(f"New value: {value}")
+    logging.info(f"New value: {value}")
     mylcd.lcd_display_string(f"New value: {value}", 1)
 
 def buttonPushed(channel):
-    print(f"button push: {channel}")
+    logging.info(f"button push: {channel}")
     mylcd.lcd_display_string(f"button push: {channel}", 2)
     time.sleep(1)
     mylcd.lcd_display_string(' '*16, 2)
     
 
 def main():
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    logging.info("[main]Program started")
 
     e1 = Encoder(27, 22, 17, valueChanged, buttonPushed)
 
     try:
+        # test xy control
+        xy.executeCmd(["home", ['x', 1], ['y', 5], ['s', 1, 5], "z0"])
         while True:
             if pl.isNewPiece():
-                print(pl.getNewCoordinate())
+                logging.info("[main]new piece: " + str(pl.getNewCoordinate()))
 
             time.sleep(0.1)
     except Exception as e:
-        print(str(e))
+        logging.info(str(e))
 
     GPIO.cleanup()
 
